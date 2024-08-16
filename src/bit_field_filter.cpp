66ffc3256708b695b0fd06b94148b947e25377d7
KPSDing <87256542+KPSDing@users.noreply.github.com>
2024-08-16 20:10:59 +0800

    Update rgb_0816

--- src/bit_field_filter.cpp
+++ src/bit_field_filter.cpp
@@ -4,9 +4,12 @@
 
 // using bitwise and to track whtat is the user's option
 void loadCase(Image *img, int8_t option) {
-    if (option & BOX_FILTER) {cout << "NONE" << endl;}
-  if (option & SOBEL_GRADIENT) {cout << "NONE" << endl;}
-
+  if (option & BOX_FILTER){
+    img->Apply_Box_Filter();
+  }
+  if (option & SOBEL_GRADIENT){
+    img->Apply_Sobel_Gradient();
+  }
   if(option &  CONTRAST_STRETCH) {
     img->Apply_Contrast_Stretching();
   }
