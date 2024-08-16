55e28946d7754a108dccdae164dcbdaf83083599
02wei02 <wendy4741@gmail.com>
2024-08-16 10:11:00 -0400

    step 4 not yet

--- inc/photo_mosaic.h
+++ inc/photo_mosaic.h
@@ -4,11 +4,16 @@
 #include "rgb_image.h"
 
 class PhotoMosaic : public RGBImage {
+private:
+	int kernalSize_h;
+	int kernalSize_w;
+	vector<Image> tile_images;
 
 public:
   PhotoMosaic();
   PhotoMosaic(int width, int height, int ***pixels);
   ~PhotoMosaic();
+  void Parting_Photo();
 };
 
 #endif
--- src/photo_mosaic.cpp
+++ src/photo_mosaic.cpp
@@ -1,8 +1,46 @@
 #include "photo_mosaic.h"
 
-PhotoMosaic::PhotoMosaic() : Image(0, 0), pixels(NULL) {}
+#define kernalSize 32
+
+PhotoMosaic::PhotoMosaic()
+    : kernalSize_h(kernalSize), kernalSize_w(kernalSize), Image(0, 0),
+      pixels(NULL) {}
 
 PhotoMosaic::PhotoMosaic(int width, int height, int ***pixels)
     : Image(width, height), pixels(pixels) {}
 
-PhotoMosaic::~PhotoMosaic() {}
+PhotoMosaic::~PhotoMosaic() { cout << "Destructor PhotoMosaic" << endl; }
+
+void PhotoMosaic::Parting_Photo() {
+  for (int y = 0; y < this->h; y += kernalSize) {
+    for (int x = 0; x < this->w; x += kernalSize) {
+      // calculate avg color in every block
+      int r_sum = 0, g_sum = 0, b_sum = 0;
+      int pixel_count = 0;
+
+      for (int dy = 0; dy < kernalSize && y + dy < h; ++dy) {
+        for (int dx = 0; dx < kernalSize && x + dx < w; ++dx) {
+          // if pixels stored as RGB style
+          r_sum += pixels[y + dy][x + dx];
+          pixel_count++;
+        }
+      }
+
+      int avg_r = r_sum / pixel_count;
+
+      // find the most fimilier style
+    }
+  }
+}
+
+int get_avg_color() const {
+  int r_sum = 0;
+  for (const auto &row : pixels) {
+    for (auto p : row) {
+      r_sum += p;
+    }
+  }
+  return r_sum / (w * h);
+}
+
+uint8_t get_pixel(int y, int x) const { return pixels[y][x]; }
